f37048d9b45ff5988caca4d01ec8c1b0b98c5f59
ssusancho <scho2024@chadwickschool.org>
2022-01-12 12:09:53 -0800

    chore: Initialize project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace scho_BinaryNumbersInCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
